8f6b9e6cb21d22faff8b8695505db5494b3ac946
Suchit02 <mishrasatya794@gmail.com>
2023-07-29 11:30:54 +0530

    slider_project

---
+++ App.js
@@ -0,0 +1,18 @@
+import { StyleSheet,View, Text, SafeAreaView } from 'react-native'
+import React from 'react'
+import Slider from './src/component/Slider'
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Slider/>
+    </SafeAreaView>
+  )
+}
+
+export default App;
+
+const styles= StyleSheet.create(
+
+     {}
+)
---
+++ src/data/index.js
@@ -0,0 +1,39 @@
+export default [
+  {
+    id: 1,
+    //img: require('../assets/watch7.jpeg'),
+    img: require('../assets/Phone11.jpg'),
+    title: 'Nothing Phone 1',
+    description: '5G Dual 128GB 8GB RAM Smartphone Factory Unlocked - Black',
+    price: '39999 Rs.',
+  },
+  {
+    id: 2,
+    // img: require('../assets/airpod.jpeg'),
+    img: require('../assets/002_.jpg'),
+    title: 'Samsung Galaxy S23 Ultra 5G',
+    description: '(Green ,12GB, 256GB Storage)',
+    price: '1,24,999 Rs.',
+  },
+  {
+    id: 3,
+    img: require('../assets/airpodmax.jpeg'),
+    title: 'AirPods Max',
+    description: 'Effortless AirPods experience',
+    price: '5499 Rs.',
+  },
+  {
+    id: 4,
+    img: require('../assets/charger.png'),
+    title: 'Charger',
+    description: "It's not magic, it's just science",
+    price: '499 Rs.',
+  },
+  {
+    id: 5,
+    img: require('../assets/lock.jpeg'),
+    title: 'Smart Lock',
+    description: 'Unlock your door with your phone',
+    price: '1999 Rs.',
+  },
+];
